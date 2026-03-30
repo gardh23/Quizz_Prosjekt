7660beee89412c423588da6d2ca6514c08fcbed5
Gard Gimse <gard@Gards-MacBook-Pro.local>
2026-03-30 15:40:58 +0200

    Fix login redirect to use React Router navigate

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
             const res = await api.post('/auth/login', { username, password })
             localStorage.setItem('token', res.data.token)
             localStorage.setItem('user', JSON.stringify(res.data.user))
-            window.location.href = '/'
+            navigate('/')
         } catch (err) {
             setError(err.response?.data?.error || 'Noe gikk galt')
         }
